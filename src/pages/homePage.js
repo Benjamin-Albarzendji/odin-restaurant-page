48ac1a80362d8205bc0474580e41da83fa04904d
Benjamin-Albarzendji <benjamin.albarzendji@gmail.com>
2022-10-30 00:31:59 +0200

    Menu Page done

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,52 +1,42 @@
-
-
 export function homePage() {
+  //Grabs the contentDiv in the document
+  const contentDiv = document.querySelector("#content");
 
-    //Grabs the contentDiv in the document
-    const contentDiv = document.querySelector('#content')
+  //HomePage Container
+  const homePageContainer = document.createElement("div");
+  homePageContainer.classList.add("homePageContainer");
 
-    //HomePage Container
-    const homePageContainer = document.createElement("div")
-    homePageContainer.classList.add("homePageContainer")
+  //Creates the image and appends it
+  const imageContainer = document.createElement("div");
+  imageContainer.className = "imageContainer";
 
-    //Creates the image and appends it 
-    const imageContainer = document.createElement("div")
-    imageContainer.className = "imageContainer"
+  const homeImage = document.createElement("img");
+  homeImage.src = "../src/images/homePage.png";
+  imageContainer.appendChild(homeImage);
 
-    const homeImage = document.createElement("img")
-    homeImage.src = "../src/images/homePage.png"
-    imageContainer.appendChild(homeImage)
+  homePageContainer.appendChild(imageContainer);
 
-    homePageContainer.appendChild(imageContainer)
+  //Header & Subheader Text
+  const headerContainer = document.createElement("div");
+  headerContainer.className = "headerContainer";
 
+  const header = document.createElement("div");
+  header.className = "header";
+  header.innerText = "Welcome to the Bite";
 
-    //Header & Subheader Text
-    const headerContainer = document.createElement("div")
-    headerContainer.className = "headerContainer"
+  const subHeader = document.createElement("div");
+  subHeader.className = "subHeader";
+  subHeader.innerText = "Discover your Tastes!";
 
-    const header = document.createElement("div")
-    header.className = "header"
-    header.innerText = "Welcome to the Bite"
+  headerContainer.appendChild(header);
+  headerContainer.appendChild(subHeader);
+  homePageContainer.appendChild(headerContainer);
 
+  //Menu Button
+  const menuButton = document.createElement("button");
+  menuButton.innerText = "Menu";
+  headerContainer.appendChild(menuButton);
 
-    const subHeader = document.createElement("div")
-    subHeader.className = "subHeader"
-    subHeader.innerText = "Discover your Tastes!"
-
-    headerContainer.appendChild(header)
-    headerContainer.appendChild(subHeader)
-    homePageContainer.appendChild(headerContainer)
-
-
-    //Menu Button
-    const menuButton=document.createElement("button")
-    menuButton.innerText = "Menu"
-    headerContainer.appendChild(menuButton)
-
-
-    //Appends the div for it to be shown in the DOM
-    contentDiv.appendChild(homePageContainer)
-
-
-
+  //Appends the div for it to be shown in the DOM
+  contentDiv.appendChild(homePageContainer);
 }
--- src/pages/menuPage.js
+++ src/pages/menuPage.js
@@ -1,63 +1,73 @@
-
-
 //Factory Function for a menu item object
 const menuItem = (name, description, price) => {
-
   return { name, description, price };
 };
 
 //Menu Object list
-const menuObjects = []
+const menuObjects = [];
 
 //The Construction of the menu objects
-const taco = menuItem("Swedish Taco", "The tastiest of Tacos", "325$")
-menuObjects.push(taco)
+const taco = menuItem("Swedish Taco", "The tastiest of Tacos", "325$");
+menuObjects.push(taco);
 
-const pizza = menuItem("Volcanic Pizza", "Straight outta Pompei", "500$")
-menuObjects.push(pizza)
+const pizza = menuItem("Volcanic Pizza", "Straight outta Pompei", "500$");
+menuObjects.push(pizza);
 
-const bigMac = menuItem("Home-Made Big Mac", "The Original Big Mac", "500$")
-menuObjects.push(bigMac)
+const bigMac = menuItem("Home-Made Big Mac", "The Original Big Mac", "500$");
+menuObjects.push(bigMac);
 
-const pasta = menuItem("Chinese Pasta Al Fredo", "No better Pasta than Pasta from the Middle-Kingdom", "222$")
-menuObjects.push(pasta)
+const pasta = menuItem(
+  "Chinese Pasta Al Fredo",
+  "No better Pasta than Pasta from the Middle-Kingdom",
+  "222$"
+);
+menuObjects.push(pasta);
 
-const avocado = menuItem("Deconstructed Avocado Toast", "The toast that has put millions in povety", "9999$")
-menuObjects.push(avocado)
+const avocado = menuItem(
+  "Deconstructed Avocado Toast",
+  "The toast that has put millions in povety",
+  "9999$"
+);
+menuObjects.push(avocado);
 
-const banhmi = menuItem("Banh Mi", "The Original, because you don't change Banh Mi", "1$")
-menuObjects.push(banhmi)
-
+const banhmi = menuItem(
+  "Banh Mi",
+  "The Original, because you don't change Banh Mi",
+  "1$"
+);
+menuObjects.push(banhmi);
 
 export function menuPage() {
-
   //Grabs the contentDiv in the document
-  const contentDiv = document.querySelector('#content')
+  const contentDiv = document.querySelector("#content");
 
   //menuPage Container
-  const menuPageContainer = document.createElement("div")
-  menuPageContainer.classList.add("menuPageContainer")
-
-
+  const menuPageContainer = document.createElement("div");
+  contentDiv.appendChild(menuPageContainer);
+  menuPageContainer.classList.add("menuPageContainer");
 
   //Menu Container
-  const menuPage = document.createElement("div")
-  menuPage.className = "menuPage"
+  const menuPage = document.createElement("div");
+  menuPageContainer.appendChild(menuPage);
+  menuPage.className = "menuPage";
 
-  menuObjects.forEach(item => {
+  //Loop through all the objects in the list
+  menuObjects.forEach((item) => {
+    const itemCard = document.createElement("div");
+    itemCard.className = "card";
 
-    const menuItemName = document.createElement("div")
-    const menuItemDescription = document.createElement("div")
-    const menuItemPrice = document.createElement("div")
+    const menuItemName = document.createElement("div");
+    itemCard.appendChild(menuItemName);
+    menuItemName.innerText = item.name;
 
+    const menuItemDescription = document.createElement("div");
+    itemCard.appendChild(menuItemDescription);
+    menuItemDescription.innerText = item.description;
 
+    const menuItemPrice = document.createElement("div");
+    itemCard.appendChild(menuItemPrice);
+    menuItemPrice.innerText = item.price;
 
+    menuPage.appendChild(itemCard);
   });
-
-
-
-
-
-
-
 }
