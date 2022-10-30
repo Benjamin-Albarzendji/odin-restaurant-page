46769f9eb3fddd2391b700ca6fbd1e532db89ba2
Benjamin-Albarzendji <benjamin.albarzendji@gmail.com>
2022-10-30 22:33:18 +0100

    Formatting 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-console.log("Helwlso Worlsdd!");
-
-import "./style/style.css"
+import "./style/style.css";
 import { homePage } from "./pages/homePage.js";
-import { menuPage } from "./pages/menuPage.js"
+import { menuPage } from "./pages/menuPage.js";
+import { about } from "./pages/about";
+import { navBar } from "./pages/navbar";
 
-
-homePage()
-menuPage()
-
-
-
-
+homePage();
+navBar();
